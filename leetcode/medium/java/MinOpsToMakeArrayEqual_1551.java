7b6f8dcdf6092eeab274b6c8afa2c8781cb77d34
aashish2096@gmail.com <aashish2096@gmail.com>
2025-08-03 16:42:03 +0530

    PROB | 1551

---
+++ leetcode/medium/java/MinOpsToMakeArrayEqual_1551.java
@@ -0,0 +1,14 @@
+package leetcode.medium.java;
+
+public class MinOpsToMakeArrayEqual_1551 {
+     public int minOperations(int n) {
+        int left = 0, right = n-1;
+        int sum = 0;
+        while(left < right){
+            sum+= right-left;
+            left++;
+            right--;
+        }
+        return sum;
+    }
+}
