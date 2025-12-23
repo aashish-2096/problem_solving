7eb8483144275fbd2c07fd2ff4d5733b313655c1
aashish2096@gmail.com <aashish2096@gmail.com>
2025-12-23 09:25:01 +0530

    Recursion Dp Stack

--- leetcode/medium/java/tree/BinaryTreeUpsideDown_156.java
+++ leetcode/medium/java/tree/BinaryTreeUpsideDown_156.java
@@ -1,21 +1,19 @@
 package tree;
-import leetcode.pojo.*;
 public class BinaryTreeUpsideDown_156 {
-    /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+  
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
     public TreeNode upsideDownBinaryTree(TreeNode root) {
         if (root == null || root.left == null)
             return root;
